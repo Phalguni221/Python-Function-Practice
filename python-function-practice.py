3f881717c7547c1eb18aa6c02513922edd12f234
Venkatesh Bhamidipati <vbhamidipati@yahoo.com>
2022-04-09 14:51:45 -0400

    python-practice

---
+++ python-function-practice.py
@@ -0,0 +1,12 @@
+def arb_args (*args):
+print(args)
+
+arb_args ('one', 'two', 'kiwi')
+
+
+#Third
+def lunch_lady(name, preference):
+    name = input('student name')
+    preference= input('lunch preference')
+    if preference != ''
+    print:("Mystery Meat")
